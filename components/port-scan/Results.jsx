9574cc3b59fccb11472b90daf0f4b7c5ffe82e74
aswin-kevin <aswinvenkat60@gmail.com>
2021-12-28 02:20:33 +0530

    added ports scanner input splitting logic

--- components/port-scan/Results.jsx
+++ components/port-scan/Results.jsx
@@ -1,81 +1,47 @@
+/* This example requires Tailwind CSS v2.0+ */
+const portsData = [
+  { ip: "127.0.0.1", port: "80,8080,443,22" },
+  { ip: "127.0.0.2", port: "6007,8089,5058" },
+];
+
 const Results = () => {
   return (
-    <div class="flex flex-col border-[1.5px] border-gray-300 rounded-md ml-9">
-      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="overflow-hidden">
-            <table class="min-w-full">
-              <thead class="bg-white border-b">
+    <div className="flex flex-col ml-9">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-100">
                 <tr>
                   <th
                     scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    #
+                    IP ADDRESS
                   </th>
                   <th
                     scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    First
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Last
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Handle
+                    OPEN PORTS
                   </th>
                 </tr>
               </thead>
-              <tbody>
-                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    1
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    Mark
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    Otto
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    @mdo
-                  </td>
-                </tr>
-                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    2
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    Jacob
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    Thornton
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    @fat
-                  </td>
-                </tr>
-                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    3
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    Larry
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    Wild
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    @twitter
-                  </td>
-                </tr>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {portsData.map((val) => (
+                  <tr key={val.ip}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="text-sm font-medium text-gray-900">
+                          {val.ip}
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{val.port}</div>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
--- components/port-scan/Targets.jsx
+++ components/port-scan/Targets.jsx
@@ -1,18 +1,74 @@
+import { useState } from "react";
+
 const Targets = () => {
+  const [ipVal, setIpVal] = useState("");
+  const [portVal, setPortVal] = useState("");
+
+  const sliceIntoChunks = (arr, chunkSize) => {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      const chunk = arr.slice(i, i + chunkSize);
+      res.push(chunk);
+    }
+    return res;
+  };
+
+  const GetValues = () => {
+    let ipArr = [];
+    let ipString = ipVal;
+
+    for (const iterator of ipString.split("\n")) {
+      for (const key in iterator.trim().split(",")) {
+        ipArr.push(key);
+      }
+    }
+
+    let portArr = [];
+    let portString = portVal;
+
+    for (const iterator of portString.split(",")) {
+      for (const key in iterator.trim().split(",")) {
+        portArr.push(key);
+      }
+    }
+
+    // ipArr = [...new Set(ipArr)];
+    // portArr = [...new Set(portArr)];
+
+    console.log(`IP value is : ${ipArr.length}`);
+    console.log(`Port value is : ${portArr.length}`);
+
+    console.log(sliceIntoChunks(ipArr, 3));
+  };
+
+  const onIpChange = (event) => {
+    event.preventDefault();
+    setIpVal(event.target.value);
+  };
+
+  const onPortChange = (event) => {
+    event.preventDefault();
+    setPortVal(event.target.value);
+  };
+
   return (
     <div className="grid grid-rows-3 grid-flow-col gap-4">
       <div className="row-span-3 ml-9">
         <div className="text-xs mb-1 font-mono text-gray-800">TARGET IPs</div>
         <textarea
+          value={ipVal}
+          onChange={onIpChange}
           className="text-sm p-1 outline-none h-40 w-128 border-[1.5px] border-blue-400 rounded-md"
-          placeholder="Example : 192.34.56.15,192.45.32.89 or 164.78.97.10/18"
+          placeholder="Example : 192.34.56.15,192.45.32.89 or 164.78.97.10/18 (comma separated or line by line)"
         ></textarea>
       </div>
       <div className="col-span-2 ml-4">
         <div className="text-xs mb-1 font-mono text-gray-800">TARGET PORTS</div>
         <textarea
+          value={portVal}
+          onChange={onPortChange}
           className="text-sm p-1 outline-none h-10 w-128 border-[1.5px] border-blue-400 rounded-md"
-          placeholder="Example : 80-120 or 80,8080,443"
+          placeholder="Example : 80-120 or 80,8080,443 (comma separated or hyphen)"
         ></textarea>
       </div>
       <div className="row-span-2 col-span-2 ml-4">
@@ -23,7 +79,7 @@ const Targets = () => {
                 type="checkbox"
                 className="form-checkbox h-4 w-4 text-gray-600"
               />
-              <span className="ml-2 text-gray-700">Version enumeration</span>
+              <span className="ml-2 text-gray-700">No Ping (-Pn)</span>
             </label>
           </div>
           <div>
@@ -32,7 +88,7 @@ const Targets = () => {
                 type="checkbox"
                 className="form-checkbox h-4 w-4 text-gray-600"
               />
-              <span className="ml-2 text-gray-700">Random agent</span>
+              <span className="ml-2 text-gray-700">SYN Scan (-sS)</span>
             </label>
           </div>
           <div>
@@ -41,7 +97,7 @@ const Targets = () => {
                 type="checkbox"
                 className="form-checkbox h-4 w-4 text-gray-600"
               />
-              <span className="ml-2 text-gray-700">Top 100 ports</span>
+              <span className="ml-2 text-gray-700">Random Agent</span>
             </label>
           </div>
           <div>
@@ -50,11 +106,14 @@ const Targets = () => {
                 type="checkbox"
                 className="form-checkbox h-4 w-4 text-gray-600"
               />
-              <span className="ml-2 text-gray-700">Banner grabbing</span>
+              <span className="ml-2 text-gray-700">Top 100 Ports</span>
             </label>
           </div>
           <div className="col-span-2 justify-self-center">
-            <button className="bg-blue-600 text-white rounded-md px-2 py-1 font-semibold text-sm shadow-lg shadow-blue-200">
+            <button
+              onClick={GetValues}
+              className="bg-blue-600 text-white rounded-md px-2 py-1 font-semibold text-sm shadow-lg shadow-blue-200"
+            >
               Launch
             </button>
           </div>
